89c284839d15336d0b9606d75f2a69a8eb99c6a4
Eirik Sletteberg <eiriksletteberg@gmail.com>
2025-07-01 18:13:05 +0200

    Add NestJS-generated boilerplate for movie, rating and user resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MovieModule } from "./movie/movie.module";
+import { UserModule } from "./user/user.module";
+import { RatingModule } from "./rating/rating.module";
 
 @Module({
-  imports: [],
+  imports: [MovieModule, UserModule, RatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
