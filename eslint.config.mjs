e30e6785a86e3a992a6ce21ca23c29af34550119
Eirik Sletteberg <eiriksletteberg@gmail.com>
2025-07-02 14:29:11 +0200

    Adjust eslint for jest tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import eslintPluginJest from "eslint-plugin-jest";
 
 export default tseslint.config(
   {
@@ -29,6 +30,16 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-floating-promises": "warn",
       "@typescript-eslint/no-unsafe-argument": "warn",
+    },
+  },
+  {
+    files: ["**/*.spec.ts", "**/*.test.ts"],
+    plugins: {
+      jest: eslintPluginJest,
+    },
+    rules: {
+      "@typescript-eslint/unbound-method": "off",
+      "jest/unbound-method": "error",
     },
   },
 );
